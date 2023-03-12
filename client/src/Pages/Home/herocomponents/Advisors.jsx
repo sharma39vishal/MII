6b95d47f947fbbcea40f596d83b439457bc9820c
Kalash Jain <kalashjain446@gmail.com>
2023-03-12 18:06:01 +0530

    /

--- client/src/Pages/Home/herocomponents/Advisors.jsx
+++ client/src/Pages/Home/herocomponents/Advisors.jsx
@@ -4,7 +4,29 @@ import SingleAdvisor from './SingleAdviosor'
 export default function Advisors() {
   return (
     <div>
-      <SingleAdvisor img={"https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"} name={"Amol Nitave"} designation={"Cheif Advisor"} about={"About Mr Amol Nitave"}/>
+      <div className="zl-staff-header">
+        <h2>
+          <div
+            style={{
+              position: "relative",
+              display: "inline-block",
+              willChange: "auto",
+              translate: "none",
+              rotate: "none",
+              scale: "none",
+              opacity: 1,
+              transform: "translate(0px, 0px)",
+              // color:'white'
+            }}
+          >
+            Advisors
+          </div>
+        </h2>
+      </div>
+      <SingleAdvisor img={"https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"} name={"Amol Nitave"} designation={"Cheif Advisor"} about={"About Mr Amol Nitave"} />
+      <SingleAdvisor img={"https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"} name={"Amol Nitave"} designation={"Cheif Advisor"} about={"About Mr Amol Nitave"} />
+      <SingleAdvisor img={"https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"} name={"Amol Nitave"} designation={"Cheif Advisor"} about={"About Mr Amol Nitave"} />
+
     </div>
   )
 }
